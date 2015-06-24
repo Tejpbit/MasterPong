2fa080d4eb7b9fcb99b44bd59078dd948ef8d187
André Samuelsson <andresamuelsson94@gmail.com>
2015-06-24 23:25:30 +0200

    refactored creation of modelinstances

--- core/src/com/masterpong/game/MasterPongGame.java
+++ core/src/com/masterpong/game/MasterPongGame.java
@@ -3,12 +3,13 @@ package com.masterpong.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.bullet.Bullet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +19,21 @@ public class MasterPongGame extends ApplicationAdapter {
 	public ModelBatch modelBatch;
 	public PerspectiveCamera cam;
 	public Model model;
-	public List<ModelInstance> instances = new ArrayList<ModelInstance>();
+	public List<ModelInstance> wallInstances = new ArrayList<ModelInstance>();
 	public Environment environment;
 	public CameraInputController camController;
 
+	public ModelInstance ballInstance;
+	public Vector3 ballVelocity = new Vector3(1, 1, 1);
+
 	@Override
 	public void create () {
+		Bullet.init();
 		modelBatch = new ModelBatch();
 
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(10f, 5f, 5f);
-		cam.lookAt(5f,5f,5f);
+		cam.position.set(0f, 0f, 0f);
+		cam.lookAt(10f,5f,5f);
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
@@ -37,31 +42,46 @@ public class MasterPongGame extends ApplicationAdapter {
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-		ModelBuilder modelBuilder = new ModelBuilder();
-		addPlanes(0, 0, 0, 20, 10, 10, modelBuilder);
+		ModelBuilder mb = new ModelBuilder();
+		mb.begin();
+		addPlanes(0, 0, 0, 20, 10, 10, mb);
+
+		mb.node().id = "ball";
+		mb.part("sphere", GL20.GL_TRIANGLES, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)))
+				.sphere(1f, 1f, 1f, 10, 10);
+
+		model = mb.end();
+
+		ballInstance = new ModelInstance(model, "ball");
+		wallInstances.add(new ModelInstance(model, "roof"));
+		wallInstances.add(new ModelInstance(model, "floor"));
+		wallInstances.add(new ModelInstance(model, "right"));
+		wallInstances.add(new ModelInstance(model, "left"));
 
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
 	}
 
 	private void addPlanes(float x0, float y0, float z0, float x1, float y1, float z1, ModelBuilder modelBuilder){
-		addPlane(x0, y0, z0,
+		addPlane("left",
+				x0, y0, z0,
 				x1, y0, z0,
 				x1, y1, z0,
 				x0, y1, z0, modelBuilder);
-		addPlane(
+		addPlane("floor",
 				x0, y0, z0,
 				x0, y0, z1,
 				x1, y0, z1,
 				x1, y0, z0,
 				modelBuilder);
-		addPlane(
+		addPlane("right",
 				x0, y1, z1,
 				x1, y1, z1,
 				x1, y0, z1,
 				x0, y0, z1,
 				modelBuilder);
-		addPlane(
+		addPlane("roof",
 				x0, y1, z0,
 				x1, y1, z0,
 				x1, y1, z1,
@@ -69,33 +89,48 @@ public class MasterPongGame extends ApplicationAdapter {
 				modelBuilder);
 	}
 
-	private void addPlane(float x00, float y00, float z00, float x10, float y10, float z10, float x11, float y11, float z11, float x01, float y01, float z01, ModelBuilder modelBuilder) {
+	private void addPlane(String id, float x00, float y00, float z00, float x10, float y10, float z10, float x11, float y11, float z11, float x01, float y01, float z01, ModelBuilder modelBuilder) {
 		Color[] colors = {Color.GREEN, Color.RED, Color.BLUE, Color.MAGENTA, Color.CYAN};
-		instances.add(
-				new ModelInstance(modelBuilder.createRect(
+
+		modelBuilder.node().id = id;
+		modelBuilder.part("plane", GL20.GL_TRIANGLES,
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal,
+				new Material(ColorAttribute.createDiffuse(colors[(int) (Math.random() * colors.length)])))
+				.rect(
 						x00, y00, z00,
 						x10, y10, z10,
 						x11, y11, z11,
 						x01, y01, z01,
-						5, 5, 5,
-						new Material(ColorAttribute.createDiffuse(colors[(int)(Math.random() * colors.length)])),
-						VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal)
-				)
-		);
+						5, 5, 5);
 	}
 
 	@Override
 	public void render() {
 		camController.update();
+		ballInstance.transform.translate(0.1f, 0.1f, 0.1f);
+		collisionHandling();
+		
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		modelBatch.begin(cam);
-		for (ModelInstance instance : instances) {
+		for (ModelInstance instance : wallInstances) {
 			modelBatch.render(instance, environment);
 		}
 		modelBatch.end();
 
+		modelBatch.render(ballInstance, environment);
+		//ballInstance.§
+	}
+
+	private void collisionHandling() {
+		Vector3 vector = new Vector3();
+		ballInstance.transform.getTranslation(vector);
+		System.out.println(vector.toString());
+
+//		for (ModelInstance instance : wallInstances) {
+//			instance.transform.
+//		}
 	}
 
 	@Override
